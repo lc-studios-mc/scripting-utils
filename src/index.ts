aa02ddfae8247d3955942e69bed6cc130912a2f2
LuckedCoronet45 <lc45@lc-studios.net>
2025-06-24 19:24:34 +0900

    add console utils
    lets you use "console" without installing @types/node

--- src/index.ts
+++ src/index.ts
@@ -2,5 +2,6 @@ export * as blockUtils from "./block.js";
 export * as damageUtils from "./damage.js";
 export * as directionUtils from "./direction.js";
 export * as entityUtils from "./entity.js";
+export * as console from "./console.js";
 export * as mathUtils from "./math.js";
 export { Vec3 } from "./vec3.js";
