e74a8974285b1b72bcc334358b8ab038fe957fc6
LuckedCoronet45 <lc45@lc-studios.net>
2025-06-29 10:23:11 +0900

    update barrel exports that screwed jsdocs on utils

---
+++ src/utils.ts
@@ -0,0 +1,7 @@
+export * as block from "./block.js";
+export * as damage from "./damage.js";
+export * as direction from "./direction.js";
+export * as entity from "./entity.js";
+export * as math from "./math.js";
+export * as misc from "./misc.js";
+export * as player from "./player.js";
